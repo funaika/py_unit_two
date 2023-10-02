8caff81aaaead1ee753038b4e38d11c91c7436ed
funaika <funaika@ssfs.org>
2023-10-02 15:09:49 -0400

    done!

--- assignment_two.py
+++ assignment_two.py
@@ -1,6 +1,6 @@
 #Kalen Funai
 #09/29/2023
-#CarBot
+#CarBotds
 
 name = input("What is your name?")
 print("Hi",name,"! Nice to meet you, I am CarBot!")
@@ -17,4 +17,12 @@ dreamCar = input("What is your dream car?")
 print("Woah a",dreamCar,"that's a cool car, good choice!")
 cost = input("What does this car cost?")
 interest = input("What is the annual interest rate for a car loan!")
+r = float(interest)/100/12
 loan = input("If you had to take out a car loan how many years would you take the loan out for?")
+n = int(loan) * 12
+monthlyPayment = (r * int(cost)) / (1 - (1 + r) ** -n)
+
+print("The monthly payment with interest will be,", round(monthlyPayment, 2))
+
+totalCost = int(monthlyPayment) * (12*int(loan))
+print("Your total cost will be", round(totalCost, 2), ",I hope that works for you!")
